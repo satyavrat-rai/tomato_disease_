9b1bf7c64f4abca29fcb4d88e67d1d2cfafd0ffe
satyavrat-rai <satyavratrai73@gmail.com>
2025-08-28 22:46:22 +0530

    Update tomato_disease.py

--- tomato_disease.py
+++ tomato_disease.py
@@ -6,7 +6,7 @@ import numpy as np
 import streamlit as st
 from PIL import Image
 
-model = load_model("tomato-cnn_model.keras")
+model = load_model("tomato_model.keras")
 
 class_names = ['Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot', 'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot', 'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus', 'Tomato_healthy']
 
@@ -30,5 +30,6 @@ if uploaded_file is not None :
     predicted_class = class_names[np.argmax(prediction)]
 
     st.write(f"**Prediction:** {predicted_class}")
+
 
 
